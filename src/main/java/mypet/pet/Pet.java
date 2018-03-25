86633058be606e04f0b60e1a9c6e6f200f70e6e4
ckurtz1 <ckurtz1@comcast.net>
2018-03-25 17:15:42 -0500

    first time

---
+++ src/main/java/mypet/pet/Pet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypet.pet;
+
+/**
+ *
+ * @author ckurt
+ */
+public class Pet {
+    String dog;
+
+    public String getDog() {
+        return dog;
+    }
+
+    public void setDog(String dog) {
+        this.dog = dog;
+    }
+    
+    
+    
+}
